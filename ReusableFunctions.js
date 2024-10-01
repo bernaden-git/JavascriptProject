16af7d3d9bb4a33112f01bd41b6438ac06bd0e0b
Berna <bernalumangyao@gmail.com>
2024-10-02 00:03:57 +0800

    updated script

--- ReusableFunctions.js
+++ ReusableFunctions.js
@@ -1,22 +1,36 @@
 // Check if odd or even numbers
-export function EvenOdd (num) {
+export function EvenOdd(num) {
 
     let rem = num % 2
-       
-    if (rem==0){
+
+    if (rem == 0) {
         return 'Even'
     }
     else {
         return 'Odd'
     }
-  
+
 }
 
-//Check is Prime number
+//Check is Prime number - cannot be exactly divided by any whole number other than itself and 1 (e.g. 2, 3, 5, 7, 11)
 
-export function PrimeNumber(num){
-    for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
-        if(num % i === 0) return false;
+export function PrimeNumber(num) {
+
+    if (num < 2) //ex. num = 6; num = 5
+        return false
+
+    const sqr = Math.sqrt(num) // ex. sqr = squareroot of 6 which is 2.449; sqr = 2.23
+
+    if (sqr % parseInt(sqr) === 0) //2.23 % 2 === 0; 23 === 0?
+        return false
+
+    else {
+        for (let i = 2; i <= parseInt(sqr); i++) { 
+            if (num % i === 0) // 5 % 2? = 1
+                return false
+        }
+
+        return num > 1
     }
-    return num > 1;    
+
 }
